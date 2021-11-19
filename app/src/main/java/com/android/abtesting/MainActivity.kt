bc79d8a4e52a146c65ae5c645ad16f5c845bb9eb
talhafaki <talhafaki05@gmail.com>
2021-11-19 14:54:17 +0300

    set a/b testing with firebase remote config

--- app/src/main/java/com/android/abtesting/MainActivity.kt
+++ app/src/main/java/com/android/abtesting/MainActivity.kt
@@ -3,12 +3,7 @@ package com.android.abtesting
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
-import androidx.core.content.ContextCompat
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.android.gms.tasks.Task
-import com.google.firebase.iid.FirebaseInstanceId
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 
@@ -30,8 +25,6 @@ class MainActivity : AppCompatActivity() {
 
         firebaseRemoteConfig.setDefaultsAsync(R.xml.remote_config_defaults)
 
-        val token = FirebaseInstanceId.getInstance().token
-
         firebaseRemoteConfig.fetchAndActivate()
             .addOnCompleteListener(
                 this
@@ -45,15 +38,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun displayScreen() {
         val textView = findViewById<TextView>(R.id.textView)
-        val message = firebaseRemoteConfig.getString(REMOTE_CONFIG_KEY.WELCOME_MESSAGE.value)
-        textView.isAllCaps =
-            firebaseRemoteConfig.getBoolean(REMOTE_CONFIG_KEY.WELCOME_MESSAGE_CAPS.value)
 
-        textView.text = message
+        val experiment = firebaseRemoteConfig.getString("text_bg_change")
+        textView.setBackgroundColor(Color.parseColor("#$experiment"))
     }
-}
-
-enum class REMOTE_CONFIG_KEY(val value: String) {
-    WELCOME_MESSAGE_CAPS("welcome_message_caps"),
-    WELCOME_MESSAGE("welcome_message")
 }
